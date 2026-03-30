1f72e9bdfc875e971e4ce0479aeb2ded09aedeef
kalindigosine <kalindigosine@lbl.gov>
2026-03-30 15:44:02 -0700

    Fixed bug stopping Channel63 from enqueing packets into FIFO, logged in VERILATOR_FEASIBILITY.md

--- chip_network_sim/larpix_v3b/src/priority_onehot.sv
+++ chip_network_sim/larpix_v3b/src/priority_onehot.sv
@@ -7,7 +7,7 @@
 function automatic logic [PL:0] priority_onehot (input logic [PL:0] vec);
     logic [PL:0] result;
     result = '0;
-    for (int i = 0; i < PL; i++)
+    for (int i = 0; i <= PL; i++)
         if (vec[i] && result == '0) result[i] = 1'b1;
     return result;
 endfunction
